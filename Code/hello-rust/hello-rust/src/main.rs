25cddefd42e75b554751ac59603472ad33565ec7
Townlake101 <townlake101@gmail.com>
2024-02-19 00:23:46 -0500

    Update main.rs

--- Code/hello-rust/hello-rust/src/main.rs
+++ Code/hello-rust/hello-rust/src/main.rs
@@ -1,6 +1,9 @@
 slint::include_modules!();
+use std::io::Write;
+
 use serialport::prelude::*;
 
+static mut PORT: Option<Box<dyn SerialPort>> = None;
 
 fn main() -> Result<(), slint::PlatformError> {
 
@@ -8,27 +11,47 @@ fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
     let ui_handle = ui.as_weak();
     let ui = ui_handle.unwrap();
+  
     
+
     ui.on_send_values(move || {
-        let ui = ui_handle.unwrap();
-        println!("{}", ui.get_xALegxH());
+    let _ui = ui_handle.unwrap();
+    let data:&str = "test";
+    let bytes_data: &[u8] = data.as_bytes();
+    unsafe {
+        if let Some(port) = &mut PORT {
+            // Write data to the serial port
+            port.write_all(bytes_data);
+        } else {
+            println!("Serial port is not open");
+        }
+    }
     });
 
     let ui_handle = ui.as_weak();
     ui.on_connect_to_port(move || {
         let ui = ui_handle.unwrap();
-        println!("{}", ui.get_portName());
-        let temp_Port = format!("{}{}", "{}", ui.get_portName());
-        //format!("{}{}", "{}", ui.get_portName());
-        let port_name = format!("{}", temp_Port); 
-        let port = serialport::open_with_settings(&port_name, &SerialPortSettings {
-            baud_rate: 9600,
-            data_bits: DataBits::Eight,
-            flow_control: FlowControl::None,
-            parity: Parity::None,
-            stop_bits: StopBits::One,
-            timeout: std::time::Duration::from_secs(1),
-        }).expect("Failed to open serial port");
+
+        let port_name = format!("{}", ui.get_portName());
+        println!("{}", port_name);
+        unsafe {
+        if let Some(port) = &mut PORT {
+            let port = match serialport::open_with_settings(&port_name, &SerialPortSettings {
+                baud_rate: 9600,
+                data_bits: DataBits::Eight,
+                flow_control: FlowControl::None,
+                parity: Parity::None,
+                stop_bits: StopBits::One,
+                timeout: std::time::Duration::from_secs(1),
+            }){
+                Ok(port) => port,
+                Err(err) => {
+                    eprintln!("Failed to open serial port: {}", err);
+                    return; // Return early if failed to open the port
+            }
+        };
+        };
+    }
     });
     ui.run()
 }
